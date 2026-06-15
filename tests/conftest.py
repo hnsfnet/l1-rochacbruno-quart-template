423dd071906c5ee17d650b853dbe49379db2c6f7
hnsfnet <hnsfnet@users.noreply.github.com>
2026-06-15 14:58:48 +0000

    ✅ Ready to clone and code.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import sys
 import pytest
 
-from project_name import create_app
-from project_name.ext.commands import populate_db
-from project_name.ext.database import db
+from l1_rochacbruno_quart_template import create_app
+from l1_rochacbruno_quart_template.ext.commands import populate_db
+from l1_rochacbruno_quart_template.ext.database import db
 
 
 @pytest.fixture(scope="session")
